9d49d3d1c732f1458d34042993df135192a85b94
RedCoyote1 <marekkrystian272@gmail.com>
2026-02-08 17:15:01 +0100

    Update MyFirstTokenV2.sol

--- contracts/MyFirstTokenV2.sol
+++ contracts/MyFirstTokenV2.sol
@@ -1,1 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract MyFirstTokenV2 {
+    string public name = "My First Token";
+    string public symbol = "MFT";
+    uint8 public decimals = 18;
+
+    uint256 public totalSupply;
+    address public owner;
+
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner");
+        _;
+    }
+
+    constructor(uint256 _initialSupply) {
+        owner = msg.sender;
+        totalSupply = _initialSupply * (10 ** uint256(decimals));
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(balanceOf[msg.sender] >= _value, "Not enough tokens");
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function mint(address _to, uint256 _value) public onlyOwner {
+        totalSupply += _value;
+        balanceOf[_to] += _value;
+        emit Transfer(address(0), _to, _value);
+    }
+
+    function burn(uint256 _value) public {
+        require(balanceOf[msg.sender] >= _value, "Not enough tokens to burn");
+
+        balanceOf[msg.sender] -= _value;
+        totalSupply -= _value;
+        emit Transfer(msg.sender, address(0), _value);
+    }
+}
